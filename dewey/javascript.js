b480cf8378b254ceca93da164a31a3b3b3cd0f46
ed-dickinson <78687922+ed-dickinson@users.noreply.github.com>
2022-10-07 16:23:21 +0100

    stylistic UPDATES

--- dewey/javascript.js
+++ dewey/javascript.js
@@ -52,10 +52,10 @@ top_levels.forEach(heading => {
 // set up deep level opening
 document.querySelectorAll('.mw-parser-output .sub-list-item b').forEach(item => {
 
-  let base_hue = item.parentNode.parentNode.getAttribute('baseHue')
-
-  let index = Array.from(item.parentNode.parentNode.children).indexOf(item.parentNode)
-  console.log(index)
+  // let base_hue = item.parentNode.parentNode.getAttribute('baseHue')
+  //
+  // let index = Array.from(item.parentNode.parentNode.children).indexOf(item.parentNode)
+  // console.log(index)
 
   // item.parentNode.style.borderColor = `hsl(${base_hue + (index * 3.6)},${saturation}%,${luminance}%)`
 
@@ -67,6 +67,29 @@ document.querySelectorAll('.mw-parser-output .sub-list-item b').forEach(item => 
 
 // remove hyperlinks
 
-document.querySelectorAll('a').forEach(link => {
+document.querySelectorAll('#bodyContent a').forEach(link => {
   link.removeAttribute('href')
+})
+
+// classer function for name and label
+const seperateNumberAndLabel = (input) => {
+  let content = input
+
+  let number = content.substring(0, 3)
+  let name = content.substring(3)
+
+  content = '<span class="number">' + number + '</span>' + '<span class="label">' + name + '</span>'
+
+  return content
+}
+
+// seperate number and label for deep list
+document.querySelectorAll('.deep-list-item').forEach(item => {
+  item.innerHTML = seperateNumberAndLabel(item.textContent)
+})
+
+// seperate number and label for deep list
+document.querySelectorAll('.sub-list-item b').forEach(item => {
+  item.innerHTML = seperateNumberAndLabel(item.innerHTML)
+
 })
